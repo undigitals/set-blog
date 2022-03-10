57cbacd376ce16c231229eaea7b5bf156a79d508
umid <undigitals@gmail.com>
2022-03-10 13:13:32 +0500

    agent finished

---
+++ src/agent.js
@@ -0,0 +1,84 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+
+const superagent = superagentPromise(_superagent, global.Promise);
+
+const API_ROOT = 'http://localhost:3000/api/';
+
+let token = null;
+const responseBody = res => res.body;
+
+const tokenPlugin = req => {
+    if (token) {
+        req.set('authorization', `Token ${token}`)
+    }
+}
+
+
+// http://localhost:3000/api/user?id
+// request.del('/users?id')
+
+
+const requests = {
+    del: url => superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    get: url => superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    put: (url, body) => superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+    post: (url, body) => superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+}
+
+// auth
+const Auth = {
+    current: () => requests.get('/user'),
+    login: (email, password) => requests.post('/users/login', { user: { email, password } }),
+    register: (username, email, password) => requests.post('/users', { user: { username, email, password } }),
+    save: user => requests.put('/user', { user })
+};
+
+// tags
+const Tags = {
+    getAll: () => requests.get('/tags')
+}
+
+//
+const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
+const omitSlug = article => Object.assign({}, article, { slug: undefined })
+
+const Articles = {
+    all: page => requests.get(`/articles?${limit(10, page)}`),
+    byAuthor: (author, page) => requests.get(`/articles?author=${author}&${limit(5, page)}`),
+    byTag: (tag, page) => requests.get(`/articles?tag=${tag}&${limit(10, page)}`),
+    del: slug => requests.del(`/articles/${slug}`),
+    favorite: slug => requests.post(`/articles/${slug}/favorite`),
+    favoriteBy: (author, page) => requests.get(`/articles?favorited=${author}&${limit(5, page)}`),
+    feed: () => requests.get(`/articles/feed?limit=10&offset=0`),
+    get: slug => requests.get(`/articles/${slug}`),
+    unfavorite: slug => requests.del(`/articles/${slug}/favorite`),
+    update: article => requests.put(`/articles/${article.slug}`, { article: omitSlug(article) }),
+    create: article => requests.post(`/articles`, { article })
+}
+
+// comment: {
+//     body: "string"
+// }
+const Comments = {
+    create: (slug, comment) => requests.post(`/articles/${slug}/comments`, { comment }),
+    forArticle: slug => requests.get(`/articles/${slug}/comments`),
+    delete: (slug, commentId) => requests.del(`/articles/${slug}/comments/${commentId}`)
+}
+
+
+const Profile = {
+    follow: username => requests.post(`/profiles/${username}/follow`),
+    get: username => requests.get(`/profiles/${username}`),
+    unfollow: username => requests.del(`/profiles/${username}/follow`)
+}
+
+
+export default {
+    Articles,
+    Auth,
+    Comments,
+    Profile,
+    Tags,
+    setToken: _token => { token = _token }
+}
