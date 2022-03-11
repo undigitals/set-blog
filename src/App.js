bce39596185e5fda6aebc9ad515b74e5ea18676b
umid <undigitals@gmail.com>
2022-03-11 10:10:49 +0500

    html to react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Editor from './components/Editor';
+import Article from './components/Article';
+import Settings from './components/Settings';
+import Profile from './components/Profile';
+// import Profile from './components/Profile';
+
 
 const App = () => {
-  return <h1>App</h1>
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="/settings" element={<Settings />} />
+        {/* <Route path="/@:username/favorites" element={<ProfileFavorites/>}/> */}
+        <Route path="/@:username" element={<Profile />} />
+      </Routes>
+    </div>
+  )
+
 }
 
 export default App;
