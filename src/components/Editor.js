572f807e6c87db3ec1cbb7d408f8d4e3005a48b1
umid <undigitals@gmail.com>
2022-03-24 09:12:01 +0500

    editor page

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,70 @@
 import React from 'react';
+import ListErrors from './ListErrors';
+import { useParams } from 'react-router-dom';
+import agent from '../agent';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    ADD_TAG,
+    EDITOR_PAGE_LOADED,
+    REMOVE_TAG,
+    ARTICLE_SUBMITTED,
+    EDITOR_PAGE_UNLOADED,
+    UPDATE_FIELD_EDITOR,
+    UPDATE_FIELD_AUTH
+} from '../constants/actionTypes';
 
 const Editor = () => {
+
+    const dispatch = useDispatch();
+    const params = useParams();
+    const editorState = useSelector(state => state.editor);
+    const updateFieldEvent = (key) => (e) => dispatch({
+        type: UPDATE_FIELD_EDITOR, key, value: e.target.value
+    })
+
+    const changeTitle = updateFieldEvent('title');
+    const changeDescription = updateFieldEvent('description');
+    const changeBody = updateFieldEvent('body');
+    const changeTagInput = updateFieldEvent('tagInput');
+
+
+    const watchForEnter = e => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            dispatch({ type: ADD_TAG })
+        }
+    }
+
+    const removeTagHandler = tag => () => {
+        dispatch({ type: REMOVE_TAG, tag })
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        const article = {
+            title: editorState.title,
+            description: editorState.description,
+            body: editorState.body,
+            tagList: editorState.tagList
+        }
+
+        const slug = { slug: editorState.articleSlug };
+        const promise = editorState.articleSlug ?
+            agent.Articles.update(Object.assign(article, slug)) :
+            agent.Articles.create(article)
+
+        dispatch({ type: ARTICLE_SUBMITTED, payload: promise })
+
+    }
+
+    React.useEffect(() => {
+        if (params.slug) {
+            dispatch({ type: EDITOR_PAGE_LOADED, payload: agent.Articles.get(params.slug) })
+        }
+        return () => {
+            dispatch({ type: EDITOR_PAGE_UNLOADED })
+        }
+    }, [])
 
     return (
 
@@ -9,23 +73,55 @@ const Editor = () => {
                 <div className="row">
 
                     <div className="col-md-10 offset-md-1 col-xs-12">
+                        <ListErrors errors={editorState.errors} />
+
                         <form>
                             <fieldset>
                                 <fieldset className="form-group">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Article Title" />
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Article Title"
+                                        value={editorState.title}
+                                        onChange={changeTitle}
+                                    />
                                 </fieldset>
                                 <fieldset className="form-group">
-                                    <input type="text" className="form-control" placeholder="What's this article about?" />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="What's this article about?"
+                                        value={editorState.description}
+                                        onChange={changeDescription}
+                                    />
                                 </fieldset>
                                 <fieldset className="form-group">
-                                    <textarea className="form-control" rows="8"
-                                        placeholder="Write your article (in markdown)"></textarea>
+                                    <textarea
+                                        className="form-control"
+                                        rows="8"
+                                        placeholder="Write your article (in markdown)"
+                                        value={editorState.body}
+                                        onChange={changeBody}
+                                    ></textarea>
                                 </fieldset>
                                 <fieldset className="form-group">
-                                    <input type="text" className="form-control" placeholder="Enter tags" />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Enter tags"
+                                        value={editorState.tagInput}
+                                        onChange={changeTagInput}
+                                        onKeyUp={watchForEnter}
+
+                                    />
                                     <div className="tag-list"></div>
                                 </fieldset>
-                                <button className="btn btn-lg pull-xs-right btn-primary" type="button">
+                                <button
+                                    className="btn btn-lg pull-xs-right btn-primary"
+                                    type="button"
+                                    disabled={editorState.inProgress}
+                                    onClick={submitForm}
+                                >
                                     Publish Article
                                 </button>
                             </fieldset>
--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,7 @@ import home from './reducers/home';
 import articleList from "./reducers/articleList";
 import article from "./reducers/article";
 import profile from "./reducers/profile";
+import editor from "./reducers/editor";
 
 const combine = combineReducers({
     auth,
@@ -12,7 +13,8 @@ const combine = combineReducers({
     home,
     articleList,
     article,
-    profile
+    profile,
+    editor
 })
 
 export default combine;
